8611779c9ff3e2fd415dfa4a32c43399b40608dc
Darkempire78 <clement.wawszczyk@gmail.com>
2020-11-16 23:28:55 +0100

    new spam logs (hastbin) + updates

--- Cogs/onMessage.py
+++ Cogs/onMessage.py
@@ -1,5 +1,6 @@
 import discord
 import json
+import requests
 
 from discord.ext import commands
 from discord.utils import get
@@ -46,9 +47,23 @@ class OnMessageCog(commands.Cog, name="on message"):
                 await message.author.send(embed = embed)
                 await message.author.kick() # Kick the user
                 await message.channel.send(f"{message.author.mention} was kicked for spamming !")
+                
                 # Logs
+                # Create a hastbin file
+                messageNumber = 0
+                logTime = datetime.now().strftime("%m/%d/%Y at %H:%M:%S")
+                logs = f"[LOGS] {self.bot.user.name.upper()} - ANTI-SPAM \n\n{message.author} ({message.author.id}) spammed in #{message.channel} the {logTime}\n\n"
+                
+                for i in  self.bot.cached_messages:
+                    if i.author.id == message.author.id and i.content != "":
+                        messageNumber += 1
+                        logs = f"{logs}\n[{messageNumber}] {i.content}"
+
+                url = 'https://hastebin.com'
+                hastbin = requests.post(f'{url}/documents', data=logs).json()
+                hastbinUrl = url + "/" + hastbin['key']
                 try:
-                    embed = discord.Embed(title = f"**{message.author} has been kicked.**", description = f"**Reason :** He spammed in {message.channel}.\n\n**__User informations :__**\n\n**Name :** {message.author}\n**Id :** {message.author.id}", color = 0xff0000)
+                    embed = discord.Embed(title = f"**{message.author} has been kicked.**", description = f"**Reason :** He spammed in {message.channel.mention}.\n\n**__User informations :__**\n\n**Name :** {message.author}\n**Id :** {message.author.id}\n\n**Logs :** {hastbinUrl}", color = 0xff0000)
                     await logChannel.send(embed = embed)
                 except:
                     pass
@@ -57,4 +72,3 @@ class OnMessageCog(commands.Cog, name="on message"):
 
 def setup(bot):
     bot.add_cog(OnMessageCog(bot))
-
